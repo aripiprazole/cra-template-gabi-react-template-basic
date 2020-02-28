b70478e4f85f674df648662a6ea0c4090c93db6b
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-02-28 15:55:14 -0300

    fix: Add forgotten id property in User entity

--- template/src/entities/User.ts
+++ template/src/entities/User.ts
@@ -2,6 +2,7 @@ import Entity from "./Entity";
 
 export default class User implements Entity {
   constructor(
+    public id: number,
     public name: string,
     public email: string,
     public password?: string
--- template/src/interfaces/User.ts
+++
@@ -1,7 +0,0 @@
-export default class User {
-  constructor(
-    public name: string,
-    public email: string,
-    public password?: string
-  ) {}
-}
